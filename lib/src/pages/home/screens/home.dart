d40bdaf837735ad92eb9e8f67717bb32b569c89a
Eduardo Guedes <eduardoguedesdev@gmail.com>
2025-06-19 01:00:43 -0300

    finish UI screen home

--- lib/src/pages/home/screens/home.dart
+++ lib/src/pages/home/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:app_finance/src/pages/home/screens/add_expanse/categorie.dart';
+import 'package:app_finance/src/pages/home/widgets/chart_home.dart';
 import 'package:app_finance/src/ui/theme/app_colors.dart';
+import 'package:app_finance/src/ui/theme/app_fonts.dart';
 import 'package:flutter/material.dart';
 
 class ScreenHome extends StatelessWidget {
@@ -8,6 +10,89 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Otimo dia,',
+                    style: AppFonts.textHint.copyWith(fontSize: 12),
+                  ),
+                  Text('Eduardo Guedes', style: AppFonts.textSubTitle),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              EGChartCardHome(
+                title: 'Despesa',
+                value: 'R\$ 748,16',
+                color: AppColors.primary09,
+              ),
+              EGChartCardHome(
+                title: 'Receita',
+                value: 'R\$ 1245,17',
+                color: AppColors.primary04,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          EGChartCardHome(
+            title: 'Saldo',
+            value: 'R\$ 497,01',
+            color: AppColors.primary06,
+            fullWidth: true,
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 80,
+                  width: MediaQuery.sizeOf(context).width * 0.95,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: AppColors.primary01,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Dados de transações',
+                            style: AppFonts.textSubTitle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
---
+++ lib/src/pages/home/widgets/chart_home.dart
@@ -0,0 +1,61 @@
+import 'package:app_finance/src/ui/theme/app_colors.dart';
+import 'package:app_finance/src/ui/theme/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+class EGChartCardHome extends StatelessWidget {
+  const EGChartCardHome({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.color,
+    this.fullWidth = false,
+  });
+  final String title;
+  final String value;
+  final Color color;
+  final bool? fullWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: fullWidth! ? 99 : 81,
+      width:
+          fullWidth!
+              ? MediaQuery.of(context).size.width * 0.95
+              : MediaQuery.of(context).size.width * 0.45,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 4,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: AppFonts.textTitle.copyWith(
+                color: AppColors.highlight001,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              value,
+              style: AppFonts.textTitle.copyWith(color: AppColors.highlight001),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
